821dd47b31ac7e0f4a7e4dc7ed231f9c0d82f0b3
kizmo04 <get.kizmo@gmail.com>
2018-07-30 15:23:51 +0900

    parallel 수정

--- mocha_test/parallel.js
+++ mocha_test/parallel.js
@@ -35,10 +35,42 @@ describe('parallel', function() {
         });
     });
 
+    it('parallel object - 객체에 대응할 수 있어야 한다.', function(done) {
+        var call_order = [];
+        async.parallel({
+            one: function(callback) {
+                setTimeout(function() {
+                    call_order.push(1);
+                    callback(null, 1);
+                }, 200);
+            },
+            two: function(callback) {
+                setTimeout(function() {
+                    call_order.push(2);
+                    callback(null, 2);
+                }, 100);
+            }
+        },
+        function(err, results){
+            assert(err === null, err + " passed instead of 'null'");
+            expect(call_order).to.eql([2,1]);
+            expect(results).to.eql({one: 1, two: 2});
+            done();
+        });
+    });
+
     it('parallel empty array', function(done) {
         async.parallel([], function(err, results){
             assert(err === null, err + " passed instead of 'null'");
             expect(results).to.eql([]);
+            done();
+        });
+    });
+
+    it('parallel empty object', function(done) {
+        async.parallel({}, function(err, results){
+            assert(err === null, err + " passed instead of 'null'");
+            expect(results).to.eql({});
             done();
         });
     });
